3a4c5d9c53b326ae02b6cfb942019e477b757c0c
steelekreis <steele.kreis@gmail.com>
2022-02-27 07:54:32 -0700

    moved schemas to root

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 
-const {typeDefs, resolvers} = require('./schemas');
+const {typeDefs, resolvers} = require('.');
 // const typeDefs = require("./schemas");
 // const resolvers = require("./schemas");
 
