497bbf7e0a57754342c8539bb5f2a768ea6e5724
steelekreis <steele.kreis@gmail.com>
2022-02-27 05:57:13 -0700

    added some extra gitignores to be safe, importing apollo server, adding token function

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,11 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
